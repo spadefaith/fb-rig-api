8026fe5fd9589e4d6f1b729d97d34a6d179166ef
cedrick <cedrickcampoto@yahoo.com>
2023-04-16 18:10:12 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 import { getAllRig, addRig } from './utils';
 
@@ -8,6 +9,17 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+
+app.use('*', cors({
+    origin: ['http://localhost:5500','https://fb-rig.pages.dev'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+}))
+app.options('*', (c) => {
+    return c.text('', 204)
+})
 
 
 app.post('/', async (c, next)=>{
